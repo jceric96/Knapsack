3a5fa1b6bb36284802b3339d0b7ef78cede30813
jceric96 <74382751+jceric96@users.noreply.github.com>
2022-09-13 15:14:55 -0300

    add another method for different knspack question

--- Main.java
+++ Main.java
@@ -14,6 +14,7 @@ public class Main {
     }
 
     public static ArrayList<ArrayList<Integer>> knapsack(int[] weights, int target) {
+        // it is unnecessary if no duplicate numbers or no order required
         // Arrays.sort(weights);
         ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> result = new ArrayList<>();
@@ -33,11 +34,8 @@ public class Main {
 
         // result.add(weights[index]);
 
-        // can't pick repeatly the same number
-        // knapsack(target - weights[index], weights, index + 1, result, results);
-        // pick repeatly the same number
+        // pick each number repeatly
         // knapsack(target - weights[index], weights, index, result, results);
-
         // result.remove(result.size() - 1);
         // knapsack(target, weights, index + 1, result, results);
 
@@ -45,21 +43,63 @@ public class Main {
         for (int i = index; i < weights.length; i++) {
             result.add(weights[i]);
 
-            // can't pick repeatly the same number
+            // don't pick each numbe repeatly
             // knapsack(target - weights[i], weights, i + 1, result, results);
 
-            // pick repeatly the same number
+            // pick each number repeatly
             knapsack(target - weights[i], weights, i, result, results);
 
             result.remove(result.size() - 1);
         }
     }
 
+    public static ArrayList<ArrayList<Integer>> knapsackII(int[] weights, int target) {
+        // sort weights before beginning if weights contains duplicate numbers
+        // or sort required
+        Arrays.sort(weights);
+        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> result = new ArrayList<>();
+        knapsackII(target, weights, 0, result, results);
+        return results;
+    }
+
+    private static void knapsackII(int target, int[] weights, int index, ArrayList<Integer> result,
+            ArrayList<ArrayList<Integer>> results) {
+        if (target == 0) {
+            results.add(new ArrayList<>(result));
+            return;
+        }
+        if (target < 0 || index >= weights.length) {
+            return;
+        }
+
+        for (int i = index; i < weights.length; i++) {
+            result.add(weights[i]);
+
+            // don't pick each number repeatly
+            knapsackII(target - weights[i], weights, i + 1, result, results);
+            result.remove(result.size() - 1);
+
+            // avoid returning same result
+            while (i < weights.length - 1 && weights[i] == weights[i + 1]) {
+                i++;
+            }
+        }
+        return;
+    }
+
     public static void main(String[] args) {
         int[] weights = new int[] { 14, 8, 7, 5, 3 };
+        int[] weights2 = new int[] { 2, 3, 3, 3, 6, 7 };
         int target = 25;
+        int target2 = 9;
         int index = 0;
+        // check knapsack true or false
         System.out.println(knapsack(target, weights, index));
+        // print knapsack results used unlimited each number times
+        // without duplicate numbers
         System.out.println(knapsack(weights, target));
+        // print knapsack results used each number once time with duplicate numbers
+        System.out.println(knapsackII(weights2, target2));
     }
 }
