9308ee5e1168d108f2fa1012afd93f92a0af2899
jceric96 <74382751+jceric96@users.noreply.github.com>
2022-09-13 13:33:40 -0300

    knapsack

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    // base case: 1.fill 2.out of bound
+    public static boolean knapsack(int target, int[] weights, int index) {
+        if (target == 0) {
+            return true;
+        }
+        if (target < 0 || index >= weights.length) {
+            return false;
+        }
+        // pick or not pick item
+        return knapsack(target - weights[index], weights, index + 1) || knapsack(target, weights, index + 1);
+    }
+
+    public static ArrayList<ArrayList<Integer>> knapsack(int[] weights, int target) {
+        // Arrays.sort(weights);
+        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> result = new ArrayList<>();
+        knapsack(target, weights, 0, result, results);
+        return results;
+    }
+
+    private static void knapsack(int target, int[] weights, int index, ArrayList<Integer> result,
+            ArrayList<ArrayList<Integer>> results) {
+        if (target == 0) {
+            results.add(new ArrayList<>(result));
+            return;
+        }
+        if (target < 0 || index >= weights.length) {
+            return;
+        }
+        result.add(weights[index]);
+        knapsack(target - weights[index], weights, index, result, results);
+        result.remove(result.size() - 1);
+        knapsack(target, weights, index + 1, result, results);
+    }
+
+    public static void main(String[] args) {
+        int[] weights = new int[] { 14, 8, 7, 5, 3 };
+        int target = 25;
+        int index = 0;
+        System.out.println(knapsack(target, weights, index));
+        System.out.println(knapsack(weights, target));
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## 0-1 Knapsack -- Recursion
+Given a knapsack which can hold s pounds of items, and a set of items with weight w1, w2, ... wn. Return whether we can pick specific items so that their total weight s.
+
+Example input: s= 20; w= [14,8,7,5,3];
+
+Example Output: True
+
+## Knapsack -- find all results of target number
+Given a set of weights numbers (W) and a target number(T), find all unique combinations in W where the weights numbers sums to T.
+
+Example input: target= 25; weights= [14,8,7,5,3];
+
+Example Output: 
+
+[[14, 8, 3], [14, 5, 3, 3], [8, 8, 3, 3, 3], [8, 7, 7, 3], [8, 7, 5, 5], [8, 5, 3, 3, 3, 3], [7, 7, 5, 3, 3], [7, 5, 5, 5, 3], [7, 3, 3, 3, 3, 3, 3], [5, 5, 5, 5, 5], [5, 5, 3, 3, 3, 3, 3]]
